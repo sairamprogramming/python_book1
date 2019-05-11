78f43797a67ee41984c646f3e3b190d622345512
Sai Ram <sairamprogramming@gmail.com>
2019-05-11 16:45:38 +0530

    completed exercise 9 in chapter 7

--- chapter7/programming_exercises/exercise9.py
+++ chapter7/programming_exercises/exercise9.py
@@ -1,9 +1,12 @@
 def main():
+    # Creating the population list.
     infile = open('USPopulation.txt', 'r')
     population = []
     for line in infile:
         population.append(int(line))
+    infile.close()
 
+    # Making a list of population differences.
     diff = []
 
     for index in range(len(population) - 1):
@@ -13,5 +16,23 @@ def main():
     total_diff = sum(diff)
 
     average_change = total_diff / len(diff)
+
+    # Finding the max year and min year.
+    max_index = 0
+    min_index = 0
+
+    for index in range(len(diff)):
+        if diff[index] > diff[max_index]:
+            max_index = index
+        if diff[index] < diff[min_index]:
+            min_index = index
+
+    # Printing the output.
+    print("The average annual change is ", average_change)            
+    print("The year with the greatest increase in population", max_index + 1950 + 1)            
+    print("The year with teh smallest increase in population", min_index + 1 + 1950)
+
+# Calling the main function.
+main()    
 
     
